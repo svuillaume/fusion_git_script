c684b773839f30a87757006102a35720dcdc681d
sam <sam@ubuntu.demo.com>
2023-04-11 16:15:02 -0400

    fusion cluster latest configuration

--- fusion_config_update.sh
+++ fusion_config_update.sh
@@ -11,23 +11,33 @@ FUSION_PASS=admin
 FUSION_AUTH=$(echo -n "$FUSION_USER:$FUSION_PASS" | base64)
 
 #raw config file
-FILE=C:\Users\vuill\Postman\files\conf_update.cfg
+#FILE=C:\Users\vuill\Postman\files\conf_update.cfg
 
 #Retrieve Cluster CLUSTER_ID
 echo "Please enter Cluster ID?"
 read CLUSTER_ID
 
 # fetch current config
-curl -k -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/raw" > config_cluster.cfg
+curl -k -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/raw" > config_cluster_running.cfg
 
 #get config version
 VERSION=$(curl -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/version")
 echo $VERSION
 
-nano ~/tmp/fusion_script/config_cluster.cfg
+md5_fusion=$(md5sum config_cluster_running.cfg | awk '{print $1}')
+md5_git=$(md5sum config_cluster_git.cfg | awk '{print $1}')
+echo $md5_fusion
+echo $md5_git
 
-# make a change 
-# change a timeout or something obvious vi config.cfg
+sleep 1
 
-#update cluster config
-curl -w '%{http_code}' -s -X POST -H 'Content-Type: text/plain' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/raw?force_reload=false&version=$VERSION" --data-binary @config_cluster.cfg
+if [ $md5_fusion -eq $md5_git ]
+then
+        echo "Git is already update witht the lastest fusion cluster config"
+else
+        cp ~/tmp/fusion_script/config_cluster_running.cfg ~/tmp/fusion_script/config_cluster_git.cfg
+	git commit -a -m "fusion cluster latest configuration"
+	git push origin main
+fi
+
+
