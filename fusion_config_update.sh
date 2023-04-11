fa8766b50dcaa3e3f08a6953ea0bbfd35addeb9e
sam <sam@ubuntu.demo.com>
2023-04-11 16:48:07 -0400

    fusion cluster latest configuration

--- fusion_config_update.sh
+++ fusion_config_update.sh
@@ -14,8 +14,10 @@ FUSION_AUTH=$(echo -n "$FUSION_USER:$FUSION_PASS" | base64)
 #FILE=C:\Users\vuill\Postman\files\conf_update.cfg
 
 #Retrieve Cluster CLUSTER_ID
-echo "Please enter Cluster ID?"
-read CLUSTER_ID
+CLUSTER_ID=$(curl -k -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/" | jq -r ".[1].id")
+
+#echo "Please enter Cluster ID?"
+#read CLUSTER_ID
 
 # fetch current config
 curl -k -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/raw" > config_cluster_running.cfg
