3442720ff3a9a0a86e935f636c683c1db526ce47
svuillaume <vuillaumes@gmail.com>
2023-04-12 19:54:41 +0000

    fusion cluster latest configuration

--- fusion_config_update.sh
+++ fusion_config_update.sh
@@ -14,28 +14,28 @@ FUSION_AUTH=$(echo -n "$FUSION_USER:$FUSION_PASS" | base64)
 #FILE=C:\Users\vuill\Postman\files\conf_update.cfg
 
 #Retrieve Cluster CLUSTER_ID
-CLUSTER_ID=$(curl -k -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/" | jq -r ".[1].id")
+#DEMO_ID=$(curl -k -s -X GET --user $LOGIN:$PASSWORD $BASE_URL/v1/clusters | jq -r 'map(select(.name | contains ("'$DEMO_NAME'"))) | .[].id')
 
-#echo "Please enter Cluster ID?"
-#read CLUSTER_ID
+echo "Please enter Cluster ID?"
+read CLUSTER_ID
 
 # fetch current config
-curl -k -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/raw" > /tmp/fusion_script/cluster.cfg
+curl -k -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/raw" > /tmp/fusion_script/fusion_script/cluster.cfg
 
 #get config version
 #VERSION=$(curl -s -X GET -H 'Content-Type: application/json' -H "Authorization: Basic $FUSION_AUTH" "$SCHEMA://$FUSION_HOSTNAME/v1/clusters/$CLUSTER_ID/services/haproxy/configuration/version")
 #echo $VERSION
 
-md5_fusion=$(md5sum /tmp/fusion_script/cluster.cfg | awk '{print $1}')
-md5_git=$(md5sum /tmp/fusion_script/cluster_git.cfg | awk '{print $1}')
+md5_fusion=$(md5sum /tmp/fusion_script/fusion_script/cluster.cfg | awk '{print $1}')
+md5_git=$(md5sum /tmp/fusion_script/fusion_script/cluster_git.cfg | awk '{print $1}')
 
 if [ $md5_fusion == $md5_git ]
 then
      echo "You have got the lastest config stored in GitHub"
 
 else
-     cp /tmp/fusion_script/cluster.cfg /tmp/fusion_script/cluster_git.cfg
-     cd /tmp/fusion_script/
+     cp /tmp/fusion_script/fusion_script/cluster.cfg /tmp/fusion_script/fusion_script/cluster_git.cfg
+     cd /tmp/fusion_script/fusion_script/
      git commit -a -m "fusion cluster latest configuration"
      git push
 fi
